fb64b49317967a09e3b92b78234591b4953e80d3
Brian Lego <legobw@gmail.com>
2020-06-23 16:27:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,8 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  hash = {
-    key = puts output 
-  }
-  output
+  hash[key]
+  
 end
 
 def update_counting_hash(hash, key)
